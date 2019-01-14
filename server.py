aeeff661287687718b8381ab0cf1c901c7d9f598
Gabor Buki <gabor.bki@gmail.com>
2019-01-14 13:01:10 +0100

    Template files.

--- server.py
+++ server.py
@@ -5,7 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    pass
+    return render_template('index.html')
+
+
+@app.route('/game')
+def game():
+    return render_template('game.html')
 
 
 if __name__ == '__main__':
